100d1051160e31311c85bfb94e52aeffc6cec3a4
MarQtie <revenant.gerozaga@gmail.com>
2026-03-19 13:55:01 +0800

    add bit vector var2 init and insert funcs

--- Concepts/Sets/Bit Vector/Variation 2/functions.h
+++ Concepts/Sets/Bit Vector/Variation 2/functions.h
@@ -9,14 +9,50 @@ typedef struct {
     unsigned int field : 8;
 } Set;
 
-void initialize(Set Set);
-void insert(Set set, int element);
-void delete(Set set, int element);
+void initialize(Set *set);
+void insert(Set *set, int element);
+void delete(Set *set, int element);
 bool find(Set set, int element);
 void Union(Set A, Set B);
 void Intersection(Set A, Set B);
 void Difference(Set A, Set B);
 void display(Set set);
 
+void initialize(Set *set){
+    set->field = 0;
+}
+
+void insert(Set *set, int element){
+    if(element < sizeof(unsigned int) * 8) {
+        unsigned char mv = 1 << element;
+        mv |= set->field;
+    }else {
+        printf("Element Out of Bounds!\n");
+    }
+}
+
+void delete(Set *set, int element){
+
+}
+
+bool find(Set set, int element){
+
+}
+
+void Union(Set A, Set B){
+
+}
+
+void Intersection(Set A, Set B){
+
+}
+
+void Difference(Set A, Set B){
+
+}
+
+void display(Set set){
+
+}
 
 #endif
